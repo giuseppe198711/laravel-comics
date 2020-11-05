7f11afcf5d4eec5897c36ed91d94b8c23c84735c
SM Boolean <samuelemadrigali1991@gmail.com>
2020-11-05 14:29:28 +0100

    fix: comics migration

--- database/migrations/2020_11_04_104652_create_comics_table.php
+++ database/migrations/2020_11_04_104652_create_comics_table.php
@@ -17,7 +17,9 @@ class CreateComicsTable extends Migration
             $table->id();
             $table->string("title", 30);
             $table->string("original_title", 50)->nullable();
-            $table->string("author", 50);
+            $table->unsignedBigInteger('author_id')->nullable();
+            // relazione
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
             $table->smallInteger('number');
             $table->smallInteger('n_pages');
             $table->string("edition", 50);
